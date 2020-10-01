0e82bf331f274f5ecd7c60edb87807916502a702
PhotolyzeAnt <none>
2020-10-01 15:57:29 +0200

    change redirect url

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 const authEndpoint = "https://accounts.spotify.com/authorize"
 const clientId = "f10fc34ec54b46fc92b4086dd5d86fcd"
-const redirectUri = "http://localhost:3000/"
+const redirectUri = "https://photolyzeant.github.io/playlist2csv/"
 
 const scopes = ["playlist-read-private"]
 
